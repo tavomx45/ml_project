7299ca493b4aa6ae6c6a7f641049ff2687ebfc6a
Gustavo Paredes <bryangustavoparedes@gmail.com>
2024-06-10 11:54:15 -0500

    data ingestion file created

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -12,7 +13,6 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_details(error=error_message, error_detail=error_detail)
-    
 
     def __str__(self) -> str:
         return self.error_message
